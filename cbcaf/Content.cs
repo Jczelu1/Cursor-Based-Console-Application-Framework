709ddffbb50dc88c1066163179348356567430f7
Jczelu1 <julian2008c@interia.pl>
2024-06-04 20:41:06 +0200

    basics

--- cbcaf/Content.cs
+++ cbcaf/Content.cs
@@ -2,12 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace cbcaf
 {
-    public class Content
+    public interface iContent 
     {
+        string? Id { get; set; }
+        char? Group { get; set; }
+    }
+    public interface iPrintable : iContent 
+    {
+        public void PrintContent();
+    }
+    public interface iSelectable : iPrintable
+    {
+        public bool IsSelectDisabled();
+        public void PrintContentSelected();
+    }
+    public interface iExecutable : iContent
+    {
+        public void Execute();
+    }
+    public class PlainText : iSelectable
+    {
+        public string? Id { get; set; }
+        public char? Group { get; set; }
+        public string Text  { get; set; }
 
+        public PlainText(string text = "", string? id = null, char? group = null)
+        {
+            Text = "";
+            Id = null;
+            Group = null;
+        }
+
+        public void PrintContent()
+        {
+            Console.WriteLine(Text);
+        }
+
+        public void PrintContentSelected()
+        {
+            Console.WriteLine(">" + Text);
+        }
+
+        public bool IsSelectDisabled()
+        {
+            return false;
+        }
     }
 }
--- cbcaf/Page.cs
+++ cbcaf/Page.cs
@@ -1,7 +1,27 @@
-﻿namespace cbcaf
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace cbcaf
 {
     public class Page
     {
+        public string? Id;
+        public char? Group;
+        public List<iContent> Contents = new List<iContent>();
 
+
+        public int Cursor { get; }
+
+        public Page(List<iContent> contents, string? id = null, char? group = null)
+        {
+            Contents = contents;
+        }
+        public Page(string? id = null, char? group = null) { }
+
+        public void Display()
+        {
+
+        }
     }
 }
