04693a079669e335fb716377de0941e4b4b12351
Jczelu1 <julian2008c@interia.pl>
2024-08-24 20:25:12 +0200

    empty

---
+++ cbcaf/Page/Empty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace cbcaf.Page
+{
+    public class Empty : IPrintable
+    {
+        public string? Id { get; set; }
+        public char? Group { get; set; }
+
+        public byte Size { get; set; }
+
+        public Empty(byte size = 1, string? id = null, char? group = null) 
+        {
+            Id = id;
+            Group = group;
+            Size = size;
+        }
+
+        public virtual void PrintContent(int width, int leftOffset)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop+Size);
+        }
+    }
+}
